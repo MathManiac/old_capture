a5d76c1715ad91ad0019b12d838040ac91208d39
Henrik Hansen <lianides@hotmail.com>
2021-01-06 20:40:24 +0100

    8. done with animation and mobile

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,30 +1,40 @@
 import React from 'react';
 import home1 from '../img/home1.png';
 //Styled
-//import styled from 'styled-components';
 import {About, Description, Image, Hide} from '../styles';
+//Framer Motion
+import {motion} from 'framer-motion';
+import { titleAnim, fade, photoAnim } from "../animation";
+import Wave from "./Wave";
 
 const AboutSection = () => {
     return(
         <About>
             <Description>
-                <div className="title">
+                <motion.div className="title">
                     <Hide>
-                        <h2>We work to make</h2>
+                        <motion.h2 variants={titleAnim}>
+                            We work to make
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>your <span>dreams</span> come</h2>
+                        <motion.h2 variants={titleAnim}>
+                            your <span>dreams</span> come
+                        </motion.h2>
                     </Hide>
                     <Hide>
-                        <h2>true.</h2>
+                        <motion.h2 variants={titleAnim}>
+                            true.
+                        </motion.h2>
                     </Hide>
-                </div>
-                <p>Contact us for any photo etc</p>
-                <button>Contact Us</button>
+                </motion.div>
+                <motion.p variants={fade}>Contact us for any photo etc</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="guy with a camera"/>
+                <motion.img variants={photoAnim}  src={home1} alt="guy with a camera"/>
             </Image>
+            <Wave />
         </About>
     )
 }
--- src/components/ServicesSection.js
+++ src/components/ServicesSection.js
@@ -8,10 +8,19 @@ import home2 from '../img/home2.png';
 //Styles
 import {About, Description, Image} from '../styles';
 import styled from 'styled-components';
+import { scrollReveal } from "../animation";
+import { useScroll } from "./useScroll";
 
 const ServicesSection = () => {
-    return(
-        <Services>
+    const [element, controls] = useScroll();
+    console.log(controls);
+    return (
+      <Services
+        variants={scrollReveal}
+        animate={controls}
+        initial="hidden"
+        ref={element}
+      >
             <Description>
                 <h2>High <span>quality</span> services</h2>
                 <Cards>
@@ -65,6 +74,9 @@ const Services = styled(About)`
 const Cards = styled.div`
     display: flex;
     flex-wrap: wrap;
+    @media (max-width: 1300px){
+           justify-content: center;
+       }
 `;
 
 const Card = styled.div`
